d0c6f62d810d6bf8c9959637e0211410d1e06a87
Trevor <pleadnocontest@gmail.com>
2014-04-16 16:46:47 -0700

    fixed the drawing, added colors

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,16 +1,40 @@
 $(document).ready(function() {
-		for (var i = 0; i <= 483; i++) {
+		for (var i = 0; i < 1600; i++) {
 	    $('<div />', {
 	        'class' : 'grid',
 	        'id'    : 'div' + i
-	    }).appendTo('#wrapper');
+	    }).appendTo('#sketch-wrapper');
 	}
 });
 
 $(document).ready(function() {
+	
+	chosenColor = "black"
+	
 	$('.grid').hover(function() {
-		$(this).css("background-color", "red");
+		$(this).css("background-color", chosenColor);
+	})
+})
+
+$(document).ready(function() {
+	
+	$('#wrapper > a').click(function() {
+		$(this).css("border", "white 2px solid")
+	})
+	
+	$('#red').click(function() {
+		chosenColor = "red"
+	})
+	$('#blue').click(function() {
+		chosenColor = "blue"
+	})
+	$('#green').click(function() {
+		chosenColor = "green"
+	})
+	$('#black').click(function() {
+		chosenColor = "black"
 	})
 	
 	
 })
+
